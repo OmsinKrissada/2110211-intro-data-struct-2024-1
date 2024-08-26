4592012fb1aac8099d0b6a621140c8691c8f72fd
Omsin <omsin_47@hotmail.com>
2024-08-26 16:54:07 +0700

    minor adjustments to tester script

--- tester.sh
+++ tester.sh
@@ -1,14 +1,23 @@
-g++-14 --std=c++17 src/$1.cpp -o bin/$1 && cat data/in.txt | ./bin/$1 > data/generated.txt || { exit 1; }
-s1=$(cat data/generated.txt | xargs)
+g++-14 --std=c++17 src/$1.cpp -o bin/$1 || { exit 1; }
+
+begin=$(date +%s)
+output=$(cat data/in.txt | ./bin/$1)
+# sleep 1
+end=$(date +%s)
+
+echo $output > data/generated.txt
+s1=$(echo $output | xargs)
 s2=$(cat data/out.txt | xargs)
 
-echo $s1 > data/generated.txt
-echo $s2 > data/out.txt
+# echo $s1 > data/generated.txt
+# echo $s2 > data/out.txt
 
 if [[ "$s1" == "$s2" ]]
 then
-	echo "Passed"
+	echo "Results matched."
 else
-	echo "Failed. To be correct, the following changes must be achieved.\n"
+	echo "Results don't match. To be correct, the following changes must be achieved.\n"
 	diff --minimal --color data/generated.txt data/out.txt
 fi
+
+echo execution took $(expr $end - $begin) seconds
