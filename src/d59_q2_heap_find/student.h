4c49a95ae786820b5e3715cc11d2df43516a216f
Krissada Singhakachain <omsin_47@hotmail.com>
2024-10-19 02:16:05 +0700

    a little faster heap find

--- src/d59_q2_heap_find/student.h
+++ src/d59_q2_heap_find/student.h
@@ -11,8 +11,16 @@
 template <typename T, typename Comp>
 bool CP::priority_queue<T, Comp>::find(T k) const {
     // your code here
+    size_t checkpoint = 2;
+    T maxPerLevel = mData[0];
     for (size_t i = 0; i < mSize; i++) {
+        if (mData[i] > maxPerLevel) maxPerLevel = mData[i];
         if (mData[i] == k) return true;
+        if (i == checkpoint) {
+            if (mLess(maxPerLevel, k)) return false;
+            maxPerLevel = mData[i + 1];
+            checkpoint = (checkpoint + 1) * 2;
+        }
     }
     return false;
 }
@@ -21,8 +29,16 @@ template <typename T, typename Comp>
 int CP::priority_queue<T, Comp>::find_level(T k) const {
     // your code here
     int maxDepth = -1;
+    size_t checkpoint = 2;
+    T maxPerLevel = mData[0];
     for (size_t i = 0; i < mSize; i++) {
-        if (mData[i] == k) maxDepth = (int)log2(i + 1);
+        if (mData[i] > maxPerLevel) maxPerLevel = mData[i];
+        if (mData[i] == k) maxDepth = log2(i + 1);
+        if (i == checkpoint) {
+            if (mLess(maxPerLevel, k)) break;
+            maxPerLevel = mData[i + 1];
+            checkpoint = (checkpoint + 1) * 2;
+        }
     }
     return maxDepth;
 }
